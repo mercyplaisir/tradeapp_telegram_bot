dc1031ea92938ec7a902221736654a3e96eae2af
Mercy Plaisir <kirangimercyplaisir@gmail.com>
2022-01-21 14:37:14 +0200

    updating

--- src/main.py
+++ src/main.py
@@ -1,21 +1,29 @@
-from pathlib import Path
+import requests
 
 from telegram.ext import *
 
-THISFOLDER = Path(__file__).resolve().parent
-TOKENFOLDER = f"{THISFOLDER}/token.txt"
-
-# with open(TOKENFOLDER, "r") as f:
-#     toke = str(f.read())
-# print(toke)
-updater = Updater(token="2097715946:AAEnO6Ce8GtgvC1Cxt4uhvV40ts2Dw3H3T0",use_context=True)
+TOKEN = "2097715946:AAEnO6Ce8GtgvC1Cxt4uhvV40ts2Dw3H3T0"
+updater = Updater(token= TOKEN,use_context=True)
 dispacher = updater.dispatcher
 
+def balance():
+    pass
+
+def status():
+    pass
+
+
+def trading_history():
+    pass
 
 def handle_message(update, context):
     update.message.reply_text(f"you said {update.message.text}")
 
 
 dispacher.add_handler(MessageHandler(Filters.text, handle_message))
+dispacher.add_handler(MessageHandler(Filters.text, trading_history))
+dispacher.add_handler(MessageHandler(Filters.text, status))
+dispacher.add_handler(MessageHandler(Filters.text, balance))
+
 
 updater.start_polling()
